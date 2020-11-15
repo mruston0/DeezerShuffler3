2eaa2f8f6b4556691850eb11049e44fedce2c0bd
Matthew Ruston <artanis0@gmail.com>
2020-11-15 16:39:39 -0500

    simple album count cache

--- api/src/http/get-randomalbum/index.py
+++ api/src/http/get-randomalbum/index.py
@@ -1,26 +1,42 @@
 import jwt
 import json
 import random
+import logging
 from vendor.shared.repositories.deezer_shuffler import DeezerShufflerRepository
-
 from jwt import algorithms
+
+logger = logging.getLogger()
+logger.setLevel(logging.INFO)
+
+user_album_counts = {}
 
 def handler(req, context):
     jwt_encoded = req.get('queryStringParameters', {}).get('token')
 
     token = jwt.decode(jwt_encoded, 'secret', algorithm='HS256')
 
+    # Cache the album counts in a variable outside of the function 
+    # context so Lambda will perist between function calls. Saves a few hits to Dynamo sometimes.
     user_id = token['id']
-    album_count = DeezerShufflerRepository().get_user_album_count(user_id)
+    if user_id not in user_album_counts:
+        album_count = DeezerShufflerRepository().get_user_album_count(user_id)
+        user_album_counts[user_id] = album_count
+    else:
+        album_count = user_album_counts[user_id]
+    
     if album_count is None:
-        print("Error: User album count is none!")
+        logger.error("Error: User album count is none!")
         return {
             "statusCode": 500
         }
+    
     album_choice = random.randrange(0, album_count)
     album = DeezerShufflerRepository().get_album(user_id, album_choice)
-    print(f"Returning album {album} for album_choice {album_choice}")
-
+    log = {
+        "album_choice": album_choice,
+        "album": f"{album['artist']['name']} - {album['title']}"
+    }
+    logger.info(json.dumps(log))
     return {
         "statusCode": 200,
         'headers': {
